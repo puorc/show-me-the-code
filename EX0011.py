c74668a54e6bfb8ef159f3067f02b89a3f81ae76
zhaopu <puorc@vip.qq.com>
2016-03-29 21:23:30 +0800

    011

---
+++ EX0011.py
@@ -0,0 +1,16 @@
+# Project : show me the code
+# Author : puorc
+
+tmp = ''
+filtered_words = []
+with open('filtered_words.txt', 'r') as file:
+    for line in file:
+        tmp += line
+filtered_words = tmp.split('\n')
+content = raw_input('Please input your advice:')
+try:
+    filtered_words.index(content)
+except ValueError:
+    print 'Human Rights'
+else:
+    print 'Freedom'
